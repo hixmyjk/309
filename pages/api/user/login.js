1866948ea8f9000f94f1c02e0a0d98115fb0375e
jxwill <90775200+jxwill@users.noreply.github.com>
2024-11-01 11:06:05 +0800

    add users
    add users for test

---
+++ pages/api/user/login.js
@@ -0,0 +1,2 @@
+// pages/api/users/login.js
+
---
+++ pages/api/user/register.js
@@ -0,0 +1,45 @@
+import prisma from "@/utils/db";
+import { hashPassword } from "@/utils/auth";
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const {firstname, lastname, email, password} = req.body;
+
+  if (!firstname || !password || !email || !lastname) {
+    return res.status(400).json({ error: 'firstname lastname email password required' });
+  }
+
+  // Hash the password and create the user in Prisma
+  hashPassword(password).then(hashedPassword => {
+    prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        password: hashedPassword,
+        email,
+      },
+    })
+    .then(user => {
+      res.status(201).json({ user :user}); // Return newly created user
+    })
+    .catch(error => {
+      console.error('Registration Error:', error);
+
+      // Handle unique constraint error for username
+      if (error.code === 'P2002' && error.meta && error.meta.target.includes('email')) {
+        return res.status(400).json({ error: 'Username already exists' });
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+  })
+  .catch(() => {
+    return res.status(500).json({ error: 'Error hashing password' });
+  })
+  .finally(() => {
+    prisma.$disconnect(); // Disconnect Prisma if necessary
+  });
+}
